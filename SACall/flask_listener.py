043d42f347b71340bf7574c3ed1d0c7f955c0fe5
Michał Ławinski <michrarr@gmail.com>
2025-03-08 20:28:36 +0100

    Pass batch size to SACall and change value 1

--- SACall/flask_listener.py
+++ SACall/flask_listener.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route('/sacall/', methods=['GET'])
 def run_script():
     input_path = request.args.get('input_path', "/jetson-basecalling/test_sample/small/")
+    signal_window_length = request.args.get('signal_window_length', 128)
     batch_size = request.args.get('batch_size', 128)
     
     try:
-        command = ['bash', 'run_basecalling.sh', input_path, str(batch_size)]
+        command = ['bash', 'run_basecalling.sh', input_path, str(signal_window_length), str(batch_size)]
         subprocess.run(command, check=True)
         return f'Running script with input: {input_path}, batch size: {batch_size}'
     except subprocess.CalledProcessError as e:
--- SACall/run_basecalling.sh
+++ SACall/run_basecalling.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 INPUT_PATH=$1
-BATCH_SIZE=$2
+SIGNAL_WINDOW_LENGTH=$2
+BATCH_SIZE=$3
 
 echo "Input Path: $INPUT_PATH"
 echo "Batch Size: $BATCH_SIZE"
 
-bash run_caller.sh ./model.chkpt "$INPUT_PATH" "$BATCH_SIZE" /output_sample/
+bash run_caller.sh ./model.chkpt "$INPUT_PATH" "$SIGNAL_WINDOW_LENGTH" /output_sample/ "$BATCH_SIZE"
 
 bash /jetson-basecalling/metrics/db/call_db.py SACALL_STATS /jetson-basecalling/SACall/execution_statistics.csv /jetson-basecalling/SACall/jetson_metrics.csv
