eb7c774072f67105e1544a3c893d462f921b1af2
Michał Ławinski <michrarr@gmail.com>
2025-04-12 11:11:50 +0200

    Chnage in RODAN sh

--- RODAN/run_basecalling.sh
+++ RODAN/run_basecalling.sh
@@ -5,4 +5,4 @@ BATCH_SIZE=$2
 echo "Input Path: $INPUT_PATH"
 echo "Batch Size: $BATCH_SIZE"
 
-bash python3 basecall.py "$INPUT_PATH" > /output_sample/output -b "$BATCH_SIZE"
+python3 basecall.py "$INPUT_PATH" > /output_sample/output -b "$BATCH_SIZE"
