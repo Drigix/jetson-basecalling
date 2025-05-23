3464633d5449f8b24e87a4a3c1706febcb0298eb
drigix <michrarr@gmail.com>
2025-05-23 21:00:18 +0200

    Update jetson mode script

--- metrics/db/add_mode_to_containers.py
+++ metrics/db/add_mode_to_containers.py
@@ -1,11 +1,11 @@
 from db_config import AzureCosmosConnector
 
 def add_mode_to_container(container_name, connector):
-    container = connector.connect_container(container_name)
-    for item in container.read_all_items():
+    connector.connect_container(container_name)
+    for item in connector.container.read_all_items():
         if 'mode' not in item:
             item['mode'] = 0  # Default value for 'mode'
-            container.replace_item(item=item, body=item)
+            connector.container.replace_item(item=item, body=item)
 
 def main():
     connector = AzureCosmosConnector()
