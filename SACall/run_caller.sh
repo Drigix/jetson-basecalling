5396dcd47df1afcd6c6003aa6f5bf3fb3c4005e4
Michał Ławinski <michrarr@gmail.com>
2025-05-24 09:48:36 +0200

    Change in sacall

--- SACall/run_caller.sh
+++ SACall/run_caller.sh
@@ -6,13 +6,14 @@ fast5_dir=$2
 signal_window_length=$3
 basecalled_filename=$4
 batch_size=$5
+jetson_node=$6
 tmp_records_dir="tmp_data_dir"
 
 # data preprocessing
 python3 generate_dataset/inference_data.py -fast5 ${fast5_dir} -records_dir ${tmp_records_dir} -raw_len ${signal_window_length}
 
 # caller
-python3 call.py -model ${model_path} -records_dir ${tmp_records_dir} -output ${basecalled_filename} -batch_size ${batch_size}
+python3 call.py -model ${model_path} -records_dir ${tmp_records_dir} -output ${basecalled_filename} -batch_size ${batch_size} -jetson_node ${jetson_node}
 
 # delete tmp records
 rm -rf ${tmp_records_dir}
