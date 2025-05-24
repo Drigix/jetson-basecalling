ee3d30ccffeeb0c4978a91ca8bdb29a052ceba0d
Michał Ławinski <michrarr@gmail.com>
2025-05-24 10:23:54 +0200

    Fix in pass argument to basecaller

--- Chiron/run_basecalling.sh
+++ Chiron/run_basecalling.sh
@@ -7,4 +7,4 @@ echo "Input Path: $INPUT_PATH"
 echo "Batch Size: $BATCH_SIZE"
 echo "Jetson mode: $JETSON_MODE"
 
-python3 /jetson-basecalling/Chiron/chiron/entry.py call -i "$INPUT_PATH" -o /output_sample/ -m /jetson-basecalling/Chiron/chiron/model/DNA_default --preset dna-pre -b "$BATCH_SIZE" "$JETSON_MODE"
+python3 /jetson-basecalling/Chiron/chiron/entry.py call -i "$INPUT_PATH" -o /output_sample/ -m /jetson-basecalling/Chiron/chiron/model/DNA_default --preset dna-pre -b "$BATCH_SIZE" --jetson_mode "$JETSON_MODE"
